2f84fc9ffd6a5c6fa809c1b2eab10af883ef120e
Joshua Ostrom <joshuaostrom@gmail.com>
2013-07-18 09:17:43 -0400

    Added support for horizontal shades via y axis
    Added support for horizontal shades via y axis.

--- flotr2.shade.js
+++ flotr2.shade.js
@@ -23,31 +23,61 @@
             a, i, shadeRanges, fillColor, fillOpacity;
 
             // TODO: More robust error checking
-            if (!options.xaxis.shade || !options.xaxis.shade.ranges) return;
+            if ((!options.xaxis || !options.xaxis.shade || !options.xaxis.shade.ranges)
+        && (!options.yaxis || !options.yaxis.shade || !options.yaxis.shade.ranges)) return;
 
             //E.fire(this.el, 'flotr:beforeshade', [this.axes.x, this.axes.y, options, this]);
 
             ctx.save();
+			// Shade the x axis
+			if(options.xaxis && options.xaxis.shade && options.xaxis.shade.ranges)
+			{
+            	a = this.axes.x;
+            	shadeRanges = options.xaxis.shade.ranges;
+           		fillColor = options.xaxis.shade.fillColor || '#838B8B';
+            	fillOpacity = options.xaxis.shade.fillOpacity || 0.5;
 
-            a = this.axes.x;
-            shadeRanges = options.xaxis.shade.ranges;
-            fillColor = options.xaxis.shade.fillColor || '#838B8B';
-            fillOpacity = options.xaxis.shade.fillOpacity || 0.5;
+            	for (i = 0; i < shadeRanges.length; i++) {
+                	var pShadeRange =
+                	[a.d2p(shadeRanges[i][0]) + plotOffset.left,
+                 	a.d2p(shadeRanges[i][1]) + plotOffset.left];
 
-            for (i = 0; i < shadeRanges.length; i++) {
-                var pShadeRange =
-                [a.d2p(shadeRanges[i][0]) + plotOffset.left,
-                 a.d2p(shadeRanges[i][1]) + plotOffset.left];
+                	ctx.fillStyle = fillColor;
+                	ctx.globalAlpha = fillOpacity;
 
-                ctx.fillStyle = fillColor;
-                ctx.globalAlpha = fillOpacity;
-
-                ctx.fillRect(
-                pShadeRange[0],
-                0 + plotOffset.top,
-                pShadeRange[1] - pShadeRange[0],
-                plotHeight + plotOffset.top);
+                	ctx.fillRect(
+               		pShadeRange[0],
+                	0 + plotOffset.top,
+                	pShadeRange[1] - pShadeRange[0],
+                	plotHeight + plotOffset.top);
+            	}
             }
+            
+            //Shade the yaxis
+			if(options.yaxis && options.yaxis.shade && options.yaxis.shade.ranges)
+			{
+            	a = this.axes.y;
+            	shadeRanges = options.yaxis.shade.ranges;
+           		fillColor = options.yaxis.shade.fillColor || '#838B8B';
+            	fillOpacity = options.yaxis.shade.fillOpacity || 0.5;
+
+            	for (i = 0; i < shadeRanges.length; i++) {
+                	var pShadeRange =
+                	[a.d2p(shadeRanges[i][0]) + plotOffset.top,
+                 	a.d2p(shadeRanges[i][1]) + plotOffset.top];
+
+                	ctx.fillStyle = fillColor;
+                	ctx.globalAlpha = fillOpacity;
+                	
+                	ctx.fillRect(
+                	0 + plotOffset.left,
+                	pShadeRange[0],
+                	plotWidth + plotOffset.left,
+                	pShadeRange[1] - pShadeRange[0]);
+            	}
+            }            
+
+            
 
             ctx.restore();
 
